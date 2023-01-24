3aa9c49ec6441efcc8f56ed64dc3ac149fdba2d2
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-01-24 16:42:49 -0700

    added api

---
+++ server /server.js
@@ -0,0 +1,3 @@
+
+// we need one line of code that that loads anything in the dotenv file
+require('dotenv').config();
--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,35 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // applicaton is a single page
+
+  //variable to store user input
+  var userInput;
+
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+
+      <div className="header-div">
+        <h3 id="main-title"> IP address tracker</h3>
+        <div className="searchBar">
+          <input  id="search-bar" type="text" placeholder="Search for any IP address or domain">
+           </input>
+           <button onClick=""> default</button>
+           
+
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+
+      <div className ="map">
+        
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
+
   )
 }
 
